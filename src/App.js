e03bcf1520f687f1668d66a484b05e6cf9f3428b
Mustaque Nadim <mustaquenadim2000@gmail.com>
2022-01-07 10:49:54 +0600

    [INIT] Project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Passionate Programmer</p>
-      </header>
+      <h1>Passionate Programmer</h1>
     </div>
   );
-}
+};
 
 export default App;
